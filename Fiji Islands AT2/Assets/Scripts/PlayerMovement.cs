a81fa8f27bc5b0436fb1d76d450ed61d3c30e0aa
jaketh20 <osbornejake06@gmail.com>
2026-03-12 22:05:15 +1100

    Adding force, stuck on modern input system
    I need to learn how to detect keyboard presses in the modern version, rather than the legacy version.

---
+++ Fiji Islands AT2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 1000f;
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey (KeyCode.W))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+}
